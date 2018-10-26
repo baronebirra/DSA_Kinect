5cb98969835d77d423a8c3b056b49bf2ba50e8e8
baronebirra <mrpridolfi@gmail.com>
2018-10-26 19:11:54 +0200

    aggiunta opzione scheletro

---
+++ Assets/Scripts/SkeletonVisible.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonVisible : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(PlayerPrefsManager.GetSkeleton());
+	}
+	
+}
